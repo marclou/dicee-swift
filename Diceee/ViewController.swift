cc8cf5350bf7181fcdd1f6953e724ab351444d9b
marclou <marc.louvion@gmail.com>
2019-02-12 18:00:56 +0800

    Initial Commit

---
+++ Diceee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diceee
+//
+//  Created by Marc Louvion on 12/02/2019.
+//  Copyright © 2019 Virallybot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
