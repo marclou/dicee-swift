0dc9fc01014176dff5c4c8ec008b495969a14c61
marclou <marc.louvion@gmail.com>
2019-02-12 18:41:24 +0800

    adding UIImageView

--- Diceee/ViewController.swift
+++ Diceee/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDice1Index: Int = 0
+    var randomDice2Index: Int = 0
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButton(_ sender: Any) {
+        randomDice1Index = Int.random(in: 0 ... 5)
+        randomDice2Index = Int.random(in: 0 ... 5)
+    }
+    
 }
 
